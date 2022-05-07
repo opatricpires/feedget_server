fa8a9079c66cc1af53a9baf0beaf864ea35b56e6
opatricpires <patric.pires@outlook.com>
2022-05-07 18:21:56 -0300

    feat(submit-feedback): add validations

--- src/use-cases/submit-feedbacks-use-case.ts
+++ src/use-cases/submit-feedbacks-use-case.ts
@@ -16,6 +16,17 @@ export class SubmitFeedbackUseCase {
   async execute(request: SubmitFeedbackUseCaseRequest) {
     const { type, comment, screenshot } = request;
 
+    if (!type) {
+      throw new Error("Type is required");
+    }
+
+    if (!comment) {
+      throw new Error("Comment is required");
+    }
+
+    if (screenshot && !screenshot.startsWith("data:image/png;base64")) {
+      throw new Error("Invalid screenshot format.");
+    }
     await this.feedbacksRepository.create({
       type,
       comment,
@@ -28,7 +39,7 @@ export class SubmitFeedbackUseCase {
         `<div>`,
         `<p>Tipo do feedback: ${type}</p>`,
         `<p>Comentário: ${comment}</p>`,
-        `</div>`,
+        screenshot ? `<img src="${screenshot}"/>` : null,
       ].join("\n"),
     });
   }
